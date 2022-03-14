b0892e12a87bc0fd8c1727493ca62ed8adbb5f63
Phil McMinn <phil@penguin.shef.ac.uk>
2022-03-14 14:51:47 +0000

    Adding login unit test to sessions/login example

---
+++ code/sessions/login/spec/unit/login.rb
@@ -0,0 +1,16 @@
+require_relative "../spec_helper"
+
+RSpec.describe "Login App" do
+  describe "GET/" do
+    it "displays the welcome message when logged in" do
+      get "/", {}, { 'rack.session' => { logged_in: true } }
+      expect(last_response.body).to include("Welcome to the Secure Area")
+    end
+
+    it "asks the user to logged in when not logged in" do
+      get "/"
+      expect(last_response).to be_redirect
+      expect(last_response.location).to end_with("/login")
+    end
+  end
+end
